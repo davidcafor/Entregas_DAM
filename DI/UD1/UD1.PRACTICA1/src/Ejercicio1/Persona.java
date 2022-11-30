dafa7902f5c43ea57fc50f1590b0d3015155aa24
davidcafor <davidcafor@icloud.com>
2022-11-30 11:09:17 +0100

    New folder & DI Practica3

---
+++ DI/UD1/UD1.PRACTICA1/src/Ejercicio1/Persona.java
@@ -0,0 +1,97 @@
+package Ejercicio1;
+
+import java.util.List;
+
+public class Persona {
+    
+    private String dni;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String sexo;
+    private List<String> permiso;
+    private String titulacion;
+
+    
+    //CONSTRUCTORES
+    
+    public Persona() {
+    }
+
+    public Persona(String dni, String nombre, String apellido1, String apellido2, String sexo, List<String> permiso, String titulacion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.sexo = sexo;
+        this.permiso = permiso;
+        this.titulacion = titulacion;
+    }
+    
+    //GETTER & SETTER
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public List<String> getPermiso() {
+        return permiso;
+    }
+
+    public void setPermiso(List<String> permiso) {
+        this.permiso = permiso;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+    
+    //TO STRING
+
+    @Override
+    public String toString() {
+        return "DNI=" + dni + ", Nombre=" + nombre + " " + apellido1 + " " + apellido2 + ", Sexo=" + sexo + ", Permiso=" + permiso + ", Titulacion=" + titulacion;
+    }
+    
+    
+}
---
+++ DI/UD3/UD3.PRACTICA1/src/Controlador/Conexion.java
@@ -0,0 +1,65 @@
+package Controlador;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author davidcf
+ */
+public class Conexion {
+    
+    public static Connection con;
+    
+    public static Connection conectarBD(){
+        
+        //CONEXION WINDOWS
+        
+        String url = "jdbc:mysql://192.168.56.101:3306/BDInformes1";
+        String user = "usuario";
+        String password = "Abc123..";
+        
+        //CONEXION MAC
+        /*
+        String url = "jdbc:mysql://localhost:3306/FIESTAS";
+        String user = "root";
+        String password = "";
+        */
+        try {
+            
+            con = DriverManager.getConnection(url, user, password);
+            return con;
+            
+        } catch (SQLException ex) { 
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            System.out.println("Error en conexion: " + e);
+        }
+        return null;
+    }
+    
+    public static Connection getCon(){
+        return con;
+    }
+    
+    public static void cerrarBD(){
+        if(con!=null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
